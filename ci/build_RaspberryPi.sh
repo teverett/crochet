dd6862acbeb4f7c935e0c4b7aace9f354e0ecb6e
Tom Everett <tom@khubla.com>
2025-01-19 11:20:54 -0700

    python

--- ci/build_RaspberryPi.sh
+++ ci/build_RaspberryPi.sh
@@ -4,8 +4,12 @@
 
 pwd
 sudo pkg install -y git
+sudo pkg install -y python
+
 sudo git clone -b releng/14.2 https://git.freebsd.org/src.git /usr/src
 ls -lat /usr/src
 sh crochet.sh -c ci/configs/config_RaspberryPi.sh
+
+# we dont want to sync the all the obj's back to the ubuntu host
 rm -rf work/obj
 
--- ci/build_i386.sh
+++ ci/build_i386.sh
@@ -6,5 +6,7 @@ sudo pkg install -y git
 sudo git clone -b releng/14.2 https://git.freebsd.org/src.git /usr/src
 ls -lat /usr/src
 sh crochet.sh -c ci/configs/config_i386.sh
+
+# we dont want to sync the all the obj's back to the ubuntu host
 rm -rf work/obj
 
