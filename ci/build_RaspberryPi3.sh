03322d7fea41dd038ba74e8149cd6f49cce77fce
Tom Everett <tom@khubla.com>
2025-01-19 14:03:11 -0700

    PI2 and 3

---
+++ ci/build_RaspberryPi3.sh
@@ -0,0 +1,16 @@
+
+
+#!/bin/sh
+
+pwd
+sudo pkg install -y git
+sudo pkg install -y python
+sudo pkg install -y u-boot-rpi
+
+sudo git clone -b releng/14.2 https://git.freebsd.org/src.git /usr/src
+ls -lat /usr/src
+sh crochet.sh -c ci/configs/config_RaspberryPi3.sh
+
+# we dont want to sync the all the obj's back to the ubuntu host
+rm -rf work/obj
+
